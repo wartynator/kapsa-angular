a8a20483206a6528645c606b8d43c530d0d4b4f9
Oleksandr <o.yuryk1324@gmail.com>
2018-05-08 14:44:38 +0200

    paginator and filtering data in table are working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatTableModule} from '@angular/material';
-import {MatSliderModule} from '@angular/material/';
+import { MatTableModule } from '@angular/material';
+import { MatSliderModule } from '@angular/material/';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
 import { VehicleService } from './services/vehicle.service';
 import { FilterTableComponent } from './filter-table/filter-table.component';
 import { FilterPanelComponent } from './filter-panel/filter-panel.component';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatTableDataSource} from '@angular/material';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatTableDataSource} from '@angular/material';
 import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
     FilterTableComponent,
-    FilterPanelComponent,  
+    FilterPanelComponent,
+    ToolbarComponent,  
   ],
   imports: [
     BrowserModule,
@@ -33,8 +37,9 @@ import { HttpModule } from '@angular/http';
     MatInputModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
-    HttpModule
-
+    HttpModule,
+    MatPaginatorModule,
+    MatToolbarModule
   ],
   providers: [VehicleService],
   bootstrap: [AppComponent]
--- src/app/filter-table/filter-table.component.ts
+++ src/app/filter-table/filter-table.component.ts
@@ -1,62 +1,93 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {Vehicle} from './../models/Vehicle';
+import { Component, OnInit, ViewChild, EventEmitter } from '@angular/core';
+import { Vehicle } from './../models/Vehicle';
 import { VehicleService } from '../services/vehicle.service';
-import {DataSource} from '@angular/cdk/collections';
+import { DataSource } from '@angular/cdk/collections';
 import { of } from 'rxjs';
-import { Observable} from 'rxjs';
-import {MatSliderModule} from '@angular/material/slider';
-import * as _ from 'lodash';  
-import {MatTableDataSource, MatSort} from '@angular/material';
+import { Observable } from 'rxjs';
+import { MatSliderModule } from '@angular/material/slider';
+import * as _ from 'lodash';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { BehaviorSubject } from 'rxjs';
-import {MatTableModule} from '@angular/material';
-
+import { MatTableModule } from '@angular/material';
+import { Sort } from '@angular/material';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-filter-table',
   templateUrl: './filter-table.component.html',
   styleUrls: ['./filter-table.component.css']
 })
-export class FilterTableComponent {  
+export class FilterTableComponent {
   vehicles: Vehicle[] = [];
-  karoserie: string[];
-  value: any;
-  selectedRychlost: number;
-  selectedVykon:number;
-  selectedHavarovane:string;
-  selectedNehavarovane:string;
-  selectedAirbagy: number;
-  selectedKaroseria: string;
-  selectedDvere: number;
+  
   //vykaslal som sa na ten export class co mame dole a idem rovno cez MatTableDataSource, teraz 
   // cielom bolo dostat pole Vehicle,co som spravil v ngOnInit
-  dataSource = new MatTableDataSource(this.vehicles);
+  dataSource: MatTableDataSource<Vehicle>;
+
   // zobrazene stlpce
-  displayedColumns = ['stav', 'vykon', 'pocet_rychlosti', 'pocet_airbagov', 'karoseria','pocet_dveri','pocet_kilometrov','rok_vyroby','model','cena','objem_motora','typ_paliva'];
-   
-  
+  displayedColumns = ['stav', 'vykon', 'pocet_rychlosti', 'pocet_airbagov', 'karoseria', 'pocet_dveri', 'pocet_kilometrov', 'rok_vyroby', 'model', 'cena', 'objem_motora', 'typ_paliva'];
+  sortedData;
+
   applyFilter(filterValue: string) {
     // tu sa vypisuje hodnota ktoru zadavas v textfield Filter
-    console.log(filterValue);
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
- constructor(private vehicleService: VehicleService) { 
-  
-}
-  @ViewChild(MatSort) sort: MatSort;
- ngOnInit() {
-  // tu subscribujem Observable<Vehicle[]> a to (vehicle) je to co mi treba cize Vehicle[] 
-  this.vehicleService.getJSON().subscribe((vehicle) => {
-    // kazdy prvok z (vehicle) pridam this.vehicles co mam v instancnej premennej
-      vehicle.forEach(element => {
-        this.vehicles.push(element);
-      });
-  });
-  this.dataSource.sort = this.sort;
- }
 
+  constructor(private vehicleService: VehicleService) {
+    this.sortedData = this.vehicles.slice();
+    this.dataSource = new MatTableDataSource(this.vehicles);
+  }
+  
+  getVehicles() {
+    return this.vehicleService.getJSON().toPromise();
+  }
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.getVehicles().then((vehicles) => {
+      this.dataSource.data = vehicles;
+    }, (e) => {
+      // You will need to handle the error here :)
+    });
+  }
+  
+  sortData(sort: Sort) {
+    //const data = this.vehicles.slice(0, this.pageEvent.pageSize);
+    const data = this.vehicles;
+    if (!sort.active || sort.direction == '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      let isAsc = sort.direction == 'asc';
+      switch (sort.active) {
+        case 'stav': return compare(a.stav, b.stav, isAsc);
+        case 'vykon': return compare(+a.vykonMotora, +b.vykonMotora, isAsc);
+        case 'pocet_rychlosti': return compare(+a.pocetRychlosti, +b.pocetRychlosti, isAsc);
+        case 'pocet_airbagov': return compare(+a.pocetAirbagov, +b.pocetAirbagov, isAsc);
+        case 'karoseria': return compare(+a.karoseria, +b.karoseria, isAsc);
+        case 'pocet_dveri': return compare(+a.pocetDveri, +b.pocetDveri, isAsc);
+        case 'pocet_kilometrov': return compare(+a.pocetKilometrov, +b.pocetKilometrov, isAsc);
+        case 'rok_vyroby': return compare(+a.rokVyroby, +b.rokVyroby, isAsc);
+        case 'model': return compare(+a.model, +b.model, isAsc);
+        case 'cena': return compare(+a.cena, +b.cena, isAsc);
+        case 'objem_motora': return compare(+a.objemMotora, +b.objemMotora, isAsc);
+        case 'typ_paliva': return compare(+a.typPaliva, +b.typPaliva, isAsc);
+        default: return 0;
+      }
+    });
+  }
 }
+function compare(a, b, isAsc) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
+
 // const VEHICLE_DATA: Vehicle[] = [{stav:'nehavarovane',vykonMotora: 271,pocetRychlosti:5,pocetAirbagov:8,karoseria:'sedan',pocetDveri:4,pocetKilometrov:125486,rokVyroby:2007,model:'',cena:71264,objemMotora:3210,typPaliva:'benzin'} ];
 
 // export class VehicleDataSource extends DataSource<any> {
